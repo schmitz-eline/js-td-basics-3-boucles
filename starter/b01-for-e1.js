027557ae86e87176d0250776dfba37961d83d8c4
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-08 12:11:41 +0200

    ex b01-for-e1

--- starter/b01-for-e1.js
+++ starter/b01-for-e1.js
@@ -33,3 +33,18 @@ EXEMPLE 1 :
 // ...
 // nbre = 10,  10 <= 10 true,        affiche 10,         nbre passe à 10+1 = 11
 // nbre = 11,  11 <= 10 FALSE,       on sort de la boucle !
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
